02a07a3a47f98ec5bea944d5db3aa15dbe44de22
GribIrina <82247357+GribIrina@users.noreply.github.com>
2021-04-22 20:45:08 +0300

    Update StaticClass.cs

--- StaticClass.cs
+++ StaticClass.cs
@@ -1,5 +1,7 @@
-// Класс для второй задачи
 using System;
+using System.IO;
+using System.Linq;
+
 namespace two
 {
     public class StaticClass
@@ -19,6 +21,19 @@ namespace two
             Random r = new Random();
             for (int i = 0; i < 20; i++)
                 mass[i] = r.Next(-10000, 10000);
+        }
+
+        public StaticClass(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                string[] s = File.ReadAllLines(filename);
+                mass = new int[s.Length];
+
+                for (int i = 0; i < s.Length; i++)
+                    mass[i] = int.Parse(s[i]);
+            }
+            else Console.WriteLine("Ошибка при считывании файла");
         }
 
         public int Solution4one()
